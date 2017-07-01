b6f23038158eec97a13157e16eb5daa0e9f91b1b
Mehmet Emin Uluyol <eminuluyol@Mehmets-MacBook-Pro.local>
2017-07-01 17:00:04 +0200

    Add Splash Fragment

---
+++ app/src/main/java/com/taurus/newyorktimes/splash/SplashFragment.java
@@ -0,0 +1,53 @@
+package com.taurus.newyorktimes.splash;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ProgressBar;
+import butterknife.BindView;
+import com.taurus.newyorktimes.R;
+
+public class SplashFragment extends BaseFragment<SplashView, SplashPresenter> implements SplashView {
+
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+
+    public static SplashFragment newInstance() {
+        return new SplashFragment();
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.fragment_splash;
+    }
+
+    @NonNull
+    @Override
+    public SplashPresenter createPresenter() {
+        return new SplashPresenter();
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getPresenter().onProgressBarShow();
+        getPresenter().onCardFeedsRequested();
+
+    }
+
+    @Override
+    public void showLoading() {
+
+        progressBar.setVisibility(View.VISIBLE);
+
+    }
+
+    @Override
+    public void hideLoading() {
+
+        progressBar.setVisibility(View.GONE);
+
+    }
+}
