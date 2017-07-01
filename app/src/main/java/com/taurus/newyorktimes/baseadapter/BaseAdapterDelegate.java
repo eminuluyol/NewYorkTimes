53fe9d664bab83e5382ea26f66cb5c346dff5ed3
Mehmet Emin Uluyol <eminuluyol@Mehmets-MacBook-Pro.local>
2017-07-01 19:52:53 +0200

    Add Endless RecyclerView

--- app/src/main/java/com/taurus/newyorktimes/baseadapter/BaseAdapterDelegate.java
+++ app/src/main/java/com/taurus/newyorktimes/baseadapter/BaseAdapterDelegate.java
@@ -2,6 +2,7 @@ package com.taurus.newyorktimes.baseadapter;
 
 import com.hannesdorfmann.adapterdelegates3.AbsListItemAdapterDelegate;
 import com.taurus.newyorktimes.baseadapter.viewholder.BaseViewHolder;
+import com.taurus.newyorktimes.listener.OnItemClickListener;
 
 public abstract class BaseAdapterDelegate<I extends T, T, VH extends BaseViewHolder>
         extends AbsListItemAdapterDelegate<I, T, VH> {
